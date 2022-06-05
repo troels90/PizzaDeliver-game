febe24464b195befa82d921d053d76af66bb6c22
Troels P <troelsp@gmail.com>
2022-06-05 15:48:39 +0200

    First commit

---
+++ Assets/Driver.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = -0.2f;
+    [SerializeField] float moveSpeed = 0.01f;
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
+        
+
+    }
+}
